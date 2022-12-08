11047c795897b2de6864951b159577f67110e409
xialc <xialc@rayfaydata.com>
2022-12-08 09:43:51 +0800

    array type assign type

--- no_type_annotations/src/main.rs
+++ no_type_annotations/src/main.rs
@@ -1,5 +1,4 @@
 #![allow(unused)]
 fn main() {
-let months = ["January", "February", "March", "April", "May", "June", "July",
-              "August", "September", "October", "November", "December"];
+let a: [i32; 5] = [1, 2, 3, 4, 5];
 }
