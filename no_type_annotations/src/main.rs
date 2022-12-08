e94cd673fca21b1fc3a8f37986bbfa87e5d1b18b
xialc <xialc@rayfaydata.com>
2022-12-08 10:06:22 +0800

    array type get

--- no_type_annotations/src/main.rs
+++ no_type_annotations/src/main.rs
@@ -1,12 +1,6 @@
-#![allow(unused)]
-
 fn main() {
-    let a = [3; 5];
-    println!("{a:?}");
-    println!("{a:#?}");
-    let a = [3, 3, 3, 3, 3];
-    println!("{a:?}");
-    println!("{a:#?}");
-    let a = [3;5, 4;5]; // expected one of `.`, `?`, `]`, or an operator
-    println!("{a:?}", );
+    let a = [1, 2, 3, 4, 5];
+
+    let first = a[0];
+    let second = a[1];
 }
