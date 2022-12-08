8ddcf728102e05481493ac5be3568bcaf632d733
xialc <xialc@rayfaydata.com>
2022-12-08 10:05:48 +0800

    array type same element

--- no_type_annotations/src/main.rs
+++ no_type_annotations/src/main.rs
@@ -7,6 +7,6 @@ fn main() {
     let a = [3, 3, 3, 3, 3];
     println!("{a:?}");
     println!("{a:#?}");
-    let a = [3;5,4;5];
+    let a = [3;5, 4;5]; // expected one of `.`, `?`, `]`, or an operator
     println!("{a:?}", );
 }
