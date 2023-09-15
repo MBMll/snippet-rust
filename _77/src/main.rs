4092a5e85c01143d7555148815e70231eca61279
xialc <xialc@rayfaydata.com>
2023-09-15 17:53:15 +0800

    add docs

--- _77/src/main.rs
+++ _77/src/main.rs
@@ -1,25 +1,26 @@
 pub struct Solution {}
 
+fn recurse(n: i32, k: i32, start_index: i32, path: Vec<i32>) -> Vec<Vec<i32>> {
+    let _length = path.len() as i32;
+    let mut result: Vec<Vec<i32>> = Vec::new();
+    if _length >= k {
+        result.push(path);
+        return result;
+    }
+    if start_index >= n {
+        return result;
+    }
+    for i in start_index..n {
+        let i = i + 1;
+        let mut path_: Vec<i32> = path.to_vec();
+        path_.push(i);
+        result.append(&mut recurse(n, k, i, path_));
+    }
+    result
+}
+
 impl Solution {
     pub fn combine(n: i32, k: i32) -> Vec<Vec<i32>> {
-        fn recurse(n: i32, k: i32, start_index: i32, path: Vec<i32>) -> Vec<Vec<i32>> {
-            let _length = path.len() as i32;
-            let mut result: Vec<Vec<i32>> = Vec::new();
-            if _length >= k {
-                result.push(path);
-                return result;
-            }
-            if start_index >= n {
-                return result;
-            }
-            for i in start_index..n {
-                let i = i + 1;
-                let mut path_: Vec<i32> = path.to_vec();
-                path_.push(i);
-                result.append(&mut recurse(n, k, i, path_));
-            }
-            result
-        }
         recurse(n, k, 0, Vec::new())
     }
 }
